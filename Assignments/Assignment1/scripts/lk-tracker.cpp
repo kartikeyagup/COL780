2e4d802192b3a56dcd067083d49e79c07a46f6eb
kartikeyagup <kartikeyagupta@hotmail.com>
2016-08-14 10:08:46 +0530

    Added camera stream opening

--- Assignments/Assignment1/scripts/lk-tracker.cpp
+++ Assignments/Assignment1/scripts/lk-tracker.cpp
@@ -1,6 +1,28 @@
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 
 int main() {
-	cv::Point3d min(0.25, 0, 0.25);
+	cv::VideoCapture cap;
+	cap.open(0);
+	cv::Mat present_frame;
+	cv::namedWindow("Tracking", 1);
+
+	if (!cap.isOpened()) {
+		std::cout << "Unable to start camera stream\n";
+		return 1;
+	}
+
+	while (1) {
+		cap >> present_frame;
+		cv::imshow("Tracking", present_frame);
+		
+		char c = (char)cv::waitKey(10);
+		if ( c== 27 ) 
+			break;
+	}
+
 	return 0;
 }
